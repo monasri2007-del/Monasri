aead2791f34f112e47a759d1680793fbf1ec87a3
monasri2007-del <monasri2007@gmail.com>
2025-09-02 13:50:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/KMonasri/pen/azvQEmb](https://codepen.io/KMonasri/pen/azvQEmb).
+
---
+++ script.js
@@ -0,0 +1,27 @@
+// Button 1 (Certificate )
+
+document.getElementById("downloadBtn1").addEventListener("click", () => {
+
+  window.open("https://acrobat.adobe.com/id/urn:aaid:sc:AP:867fe20f-df06-4772-ba27-00c45e658bb8");
+
+downloadCertificate("https://acrobat.adobe.com/id/urn:aaid:sc:AP:867fe20f-df06-4772-ba27-00c45e658bb8");
+
+});
+// Button 2 (Certificate )
+
+document.getElementById("downloadBtn2").addEventListener("click", () => {
+
+  window.open("https://acrobat.adobe.com/id/urn:aaid:sc:AP:41dfdc80-260f-43b0-923a-a532339abce1");
+
+downloadCertificate("https://acrobat.adobe.com/id/urn:aaid:sc:AP:41dfdc80-260f-43b0-923a-a532339abce1");
+
+});
+// Button 3 (Certificate )
+
+document.getElementById("downloadBtn3").addEventListener("click", () => {
+
+  window.open("https://acrobat.adobe.com/id/urn:aaid:sc:AP:6af547c2-b309-456c-902f-d643523705fd");
+
+downloadCertificate("https://acrobat.adobe.com/id/urn:aaid:sc:AP:6af547c2-b309-456c-902f-d643523705fd");
+
+});
